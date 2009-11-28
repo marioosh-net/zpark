eb33b3e08003186bb79bf98beca8ad86ae72c78a
marioosh <marioosh@963358be-e051-4187-90f4-1164e00299d6>
2009-11-28 00:42:51 +0000

    git-svn-id: file:///var/svn/java/net.marioosh.zpark@62 963358be-e051-4187-90f4-1164e00299d6

--- src/main/java/ui/ContentComposer.java
+++ src/main/java/ui/ContentComposer.java
@@ -3,6 +3,7 @@ package ui;
 import org.zkoss.zul.Div;
 import org.zkoss.zul.Label;
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
 
@@ -14,12 +15,14 @@ public class ContentComposer extends GenericForwardComposer {
 	}	
 	
 	public void onLoginLogout$div1() {
+		div1.getChildren().clear();
 		if(session.getAttribute("user") != null) {
 			System.out.println("logged 2");
-			div1.appendChild(new Label("logged"));
+			//div1.appendChild(new Label("logged"));
+			Executions.createComponents("data.zul", div1, null);
 		} else {
 			System.out.println("logged out 2");
-			div1.appendChild(new Label("logged out"));
+			div1.appendChild(new Label("Please login"));
 		}		
 	}
 }
