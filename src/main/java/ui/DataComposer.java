621f8b05fb9146805a0cffe9dc086f3740adcc81
marioosh <marioosh@963358be-e051-4187-90f4-1164e00299d6>
2009-12-04 13:52:08 +0000

    zastosowanie RowRendere'ra
    git-svn-id: file:///var/svn/java/net.marioosh.zpark@92 963358be-e051-4187-90f4-1164e00299d6

--- src/main/java/ui/DataComposer.java
+++ src/main/java/ui/DataComposer.java
@@ -5,16 +5,25 @@ import java.util.List;
 
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventListener;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
 import org.zkoss.zkplus.databind.AnnotateDataBinder;
 import org.zkoss.zul.Grid;
+import org.zkoss.zul.Label;
+import org.zkoss.zul.Messagebox;
+import org.zkoss.zul.Row;
+import org.zkoss.zul.RowRenderer;
+import org.zkoss.zul.Rows;
+import org.zkoss.zul.SimpleListModel;
 import org.zkoss.zul.Tab;
 import org.zkoss.zul.Tabbox;
 import org.zkoss.zul.Tabpanel;
 import dao2.*;
+
 /*
  * Komposer do tabelek w zakladkach
  */
+
 public class DataComposer extends GenericForwardComposer {
 	private Tabbox tabbox;
 	private Tabpanel tabClients;
@@ -22,7 +31,8 @@ public class DataComposer extends GenericForwardComposer {
 	private Grid grid_clients;
 	private Grid grid_autos;
 	private Grid grid_times;
-	
+	private Row myrow;
+
 	AnnotateDataBinder binder;
 
 	public void doAfterCompose(Component comp) throws Exception {
@@ -32,17 +42,49 @@ public class DataComposer extends GenericForwardComposer {
 		//grid_clients.setModel(new ListModelList(getAllClients()));
 		//grid_autos.setModel(new ListModelList(getAllAutos()));
 		//grid_times.setModel(new ListModelList(getAllTimes()));
-		
+
 		// TO JEST WAZNE - zaladuj dane z bindera
 		//http://www.zkoss.org/forum/listComment/6263/			
 		binder = new AnnotateDataBinder(comp);
-		binder.loadAll();			
+		binder.loadAll();
+
+		// Grid dla kielntow komplekosowo
+		/*
+		 * tak mozna tez ustawic model i renderer
+		grid_clients.setRowRenderer(new ClientRowRenderer());
+		grid_clients.setModel(new SimpleListModel(getAllClients()));
+		*/
+		Rows rs = grid_clients.getRows();
+		for (Row row : (List<Row>) rs.getChildren()) {
+			row.addEventListener("onClick", new EventListener() {
+				public void onEvent(Event event) throws Exception {
+					Row row = (Row) event.getTarget();
+					Messagebox.show("ROW: " + event.getTarget());
+					for (Object o : row.getChildren()) {
+						// labels
+					}
+				}
+			});
+		}
+
+	}
+	
+	public ClientRowRenderer getRenderer() {
+		return new ClientRowRenderer();
 	}
 
+	public void test(Event evt) throws Exception {
+		Messagebox.show("ssss:" + evt.getTarget());
+	}
+	
 	public void onCreate$tabbox() throws Exception {
 		System.out.println("onCreate$tabbox");
 	}
-
+	/*
+		public void onClick$myrow(Event event) throws Exception {
+			Messagebox.show("ROW: " + event.getTarget());
+		}
+	*/
 	public void onClick$tab1() throws Exception {
 		System.out.println("onClick$tab1");
 	}
@@ -72,5 +114,15 @@ public class DataComposer extends GenericForwardComposer {
 		System.out.println("getAllTimes();");
 		TimeHome c = new TimeHome();
 		return c.findAll();
+	}
+}
+
+class ClientRowRenderer implements RowRenderer {
+	public void render(Row row, Object data) {
+		Client client = (Client) data;
+		new Label(client.getName()).setParent(row);
+		new Label(client.getSurname()).setParent(row);
+		new Label(client.getPesel()).setParent(row);
+		row.setHeight("30px");
 	}
 }
